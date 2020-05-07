0231c34d0d97e146a4190c40f15a4c72e080462b
windo <windo@DESKTOP-JRNP2PI>
2020-05-07 16:58:00 +0900

    ResponseEntiy type

--- ex01/src/main/java/org/zerock/controller/SampleController.java
+++ ex01/src/main/java/org/zerock/controller/SampleController.java
@@ -5,6 +5,9 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -114,5 +117,16 @@ public class SampleController {
 		dto.setName("뽀야미");
 		
 		return dto;
+	}
+	@GetMapping("/ex07")
+	public ResponseEntity<String> ex07(){
+		log.info("/ex07........");
+		
+		String msg ="{\"name\": \"홍길동\"}";
+		
+		HttpHeaders header = new HttpHeaders();
+		header.add("Content-Type", "application/json;charset=UTF-8");
+		
+		return new ResponseEntity<>(msg, header, HttpStatus.OK);
 	}
 }
